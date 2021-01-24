07846c0b3a82af72f6fa9f4f676d7981927281b0
Andreas Mårtensson <andreas@addem.se>
2021-01-24 14:52:26 +0100

    ip

--- clusterconfig/cilium.sh
+++ clusterconfig/cilium.sh
@@ -2,3 +2,7 @@
 
 kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.9/install/kubernetes/quick-install.yaml
 kubectl apply -f https://raw.githubusercontent.com/cilium/cilium/v1.9/install/kubernetes/quick-hubble-install.yaml
+
+# perhaps?
+# https://archive.fosdem.org/2020/schedule/event/containers_bpf/
+# https://docs.cilium.io/en/v1.9/gettingstarted/kubeproxy-free/
