858f9f68c7a49792569ee101819be4def8305bb6
Andreas Mårtensson <andreas@addem.se>
2021-01-26 22:08:30 +0100

    macvlan

---
+++ clusterconfig/dhcp-daemon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo transactional-update shell
+    cd /etc/systemd/system
+    curl -sLO https://raw.githubusercontent.com/containernetworking/plugins/master/plugins/ipam/dhcp/systemd/cni-dhcp.socket
+    curl -sLO https://raw.githubusercontent.com/containernetworking/plugins/master/plugins/ipam/dhcp/systemd/cni-dhcp.service
+    sed -i 's#/opt/cni/bin#/usr/libexec/cni#' cni-dhcp.service
+    systemctl enable cni-dhcp.socket
+    exit
+sudo reboot
--- clusterconfig/kured.sh
+++ clusterconfig/kured.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f https://github.com/weaveworks/kured/releases/download/1.6.1/kured-1.6.1-dockerhub.yaml
 
-# Configure Kubic to trigger kured:
-# echo "REBOOT_METHOD=kured" > /etc/transactional-update.conf
+Configure Kubic to trigger kured:
+echo "REBOOT_METHOD=kured" > /etc/transactional-update.conf
 
