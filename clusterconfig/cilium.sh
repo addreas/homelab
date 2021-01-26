01fec14edbb60639d6c91863f58540838bc5bb1a
Andreas Mårtensson <andreas@addem.se>
2021-01-26 21:28:39 +0100

    testing

--- clusterconfig/cilium.sh
+++ clusterconfig/cilium.sh
@@ -6,3 +6,27 @@ kubectl apply -f https://raw.githubusercontent.com/cilium/cilium/v1.9/install/ku
 # perhaps?
 # https://archive.fosdem.org/2020/schedule/event/containers_bpf/
 # https://docs.cilium.io/en/v1.9/gettingstarted/kubeproxy-free/
+
+cat << EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1beta1
+kind: Ingress
+metadata:
+  name: hubble-ui
+  namespace: kube-system
+  annotations:
+    cert-manager.io/cluster-issuer: addem-se-letsencrypt
+    ingress.kubernetes.io/force-ssl-redirect: "true"
+spec:
+  rules:
+    - host: hubble.addem.se
+      http:
+        paths:
+          - path: /
+            backend:
+              serviceName: hubble-ui
+              servicePort: 8443
+  tls:
+    - hosts:
+        - hubble.addem.se
+      secretName: hubble-cert
+EOF
