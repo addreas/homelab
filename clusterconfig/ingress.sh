162fca77764c9a2ab3ec60c0608294a03950be95
Andreas Mårtensson <andreas@addem.se>
2021-02-04 22:52:16 +0100

    use helmrelease where available

--- clusterconfig/ingress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.43.0/deploy/static/provider/cloud/deploy.yaml
--- clusterconfig/sealed-secrets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.14.1/controller.yaml
