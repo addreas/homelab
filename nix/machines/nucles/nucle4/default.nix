697ed2ad545c0ad4d7053c16aa62e788d660f784
Andreas Mårtensson <andreas@addem.se>
2025-05-07 21:57:34 +0200

    cleanup

--- nix/machines/nucles/nucle4/default.nix
+++ nix/machines/nucles/nucle4/default.nix
@@ -7,14 +7,5 @@
 
   networking.hostName = "nucle4";
   services.tailscale.enable = true;
-
-  systemd.network.networks."00-loop" = {
-    matchConfig.Name = "lo";
-    address = [ "192.168.0.192/32" ];
-  };
-
-  networking.hosts = {
-    "192.168.0.192" = ["nucle4.localdomain" "nucles.localdomain"];
-  };
 }
 
