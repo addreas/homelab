b7924ed5ef6fd322fb8b033a03f3fd8f18cc0c15
Andreas Mårtensson <andreas@addem.se>
2025-12-29 21:08:24 +0100

    short name mode

--- nix/packages/kube/crio.nix
+++ nix/packages/kube/crio.nix
@@ -7,6 +7,7 @@
       storageDriver = "btrfs";
       settings.crio.network.plugin_dirs = [ "/opt/cni/bin" "${pkgs.cni-plugins}/bin" ];
       settings.crio.runtime.default_env = [ "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/run/wrappers/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin" ];
+      settings.crio.image.short_name_mode = "disabled";
     };
 
     system.activationScripts.var-lib-crio = "mkdir -p /var/lib/crio";
