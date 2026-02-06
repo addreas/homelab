e71abfe5e2e6da7f10027a44dfed5fc997c147be
Andreas Mårtensson <andreas@addem.se>
2026-02-06 15:22:32 +0100

    export

--- resources/system/monitoring/kube-prometheus/export.sh
+++ resources/system/monitoring/kube-prometheus/export.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+set -eu
+
+command -v jsonnet >/dev/null 2>&1 || go install github.com/google/go-jsonnet/cmd/jsonnet@latest
+command -v jb      >/dev/null 2>&1 || go install github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb@latest
+command -v cue     >/dev/null 2>&1 || go install cuelang.org/go/cmd/cue@latest
+
+export PATH="$PATH:$(go env GOPATH 2>/dev/null)/bin"
+
+cd "$(dirname "$0")"
+
+# renovate: depName=prometheus-operator/kube-prometheus datasource=github-releases
+VERSION=v0.16.0
 
 if [ ! -d vendor ]; then
   jb init
-  jb install github.com/prometheus-operator/kube-prometheus/jsonnet/kube-prometheus@main
+  jb install "github.com/prometheus-operator/kube-prometheus/jsonnet/kube-prometheus@${VERSION}"
 fi
 
 jsonnet -J vendor -m manifests -c export.jsonnet
